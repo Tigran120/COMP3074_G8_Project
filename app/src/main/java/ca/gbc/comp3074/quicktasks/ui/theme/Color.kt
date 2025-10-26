c26aa0db5fbb64f2e3a8e687848f2a7ef7377002
Tigran120 <Tigrankhachaturyan@georgebrown.ca>
2025-10-26 15:06:40 -0400

    Theme files

---
+++ app/src/main/java/ca/gbc/comp3074/quicktasks/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package ca.gbc.comp3074.quicktasks.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val TaskBlue = Color(0xFF2196F3)
+val TaskGreen = Color(0xFF4CAF50)
+val TaskOrange = Color(0xFFFF9800)
+val TaskRed = Color(0xFFF44336)
+val TaskGray = Color(0xFF9E9E9E)
